dffa6c117590f3589f218f7e532526d486b2eccd
juliethron <juliethron@gmail.com>
2026-02-12 20:40:30 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,13 +9,26 @@ const API_URL = "https://v2.api.noroff.dev/online-shop";
 let products = [];
 let cart = [];
 
+/* ✅ ADD IMAGES HERE */
+const customImages = [
+  "bilds/thething.jpg",
+  "bilds/hereditary.jpg",
+  "bilds/longlegs.jpg",
+  "bilds/suspiria.jpg",
+  "bilds/weapons.jpg",
+  "bilds/halloween.jpg"
+];
 
 async function fetchProducts() {
     try {
     const response = await fetch(API_URL);
     const json = await response.json();
 
-    products = json.data.slice(0, 6);
+    /* ✅ ATTACH IMAGES HERE */
+    products = json.data.slice(0, 6).map((product, index) => ({
+      ...product,
+      customImage: customImages[index]
+    }));
 
     renderProducts(products);
     } catch (error) {
@@ -24,13 +37,12 @@ async function fetchProducts() {
     }
 }
 
-
 function renderProducts(items) {
     grid.innerHTML = items.map(product => `
     <div class="card" onclick="openProduct('${product.id}')">
         <div
         class="poster"
-        style="background-image:url('${product.image.url}')">
+        style="background-image:url('${product.customImage}')">
         </div>
 
         <div class="card-content">
@@ -51,43 +63,3 @@ function renderProducts(items) {
     </div>
     `).join("");
 }
-
-
-function openProduct(id) {
-    window.location.href = `product.html?id=${id}`;
-}
-
-
-function addToCart(id) {
-    const product = products.find(p => p.id === id);
-    if (!product) return;
-
-    cart.push(product);
-    countEl.textContent = cart.length;
-    renderCart();
-}
-
-function renderCart() {
-    cartItems.innerHTML = cart.map(item => `
-    <div class="cart-item">
-        <span>${item.title}</span>
-        <span>${item.discountedPrice.toFixed(2)} CR</span>
-    </div>
-    `).join("");
-}
-
-
-cartBtn.addEventListener("click", () => {
-    cartPanel.classList.toggle("open");
-});
-
-
-const browseBtn = document.getElementById("browseBtn");
-
-browseBtn.addEventListener("click", () => {
-  grid.scrollIntoView({ behavior: "smooth" });
-});
-
-
-
-fetchProducts();
